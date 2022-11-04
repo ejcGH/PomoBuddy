604a8f6d7b03e2db914c72b910a1b58c420d456b
ejcGH <iam3jc@gmail.com>
2022-11-03 22:56:08 -0400

    created Play, Pause and Settings component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Timer from './Components/Timer'
 import StartButton from './Components/StartButton';
+import PauseButton from './Components/PauseButton';
+import SettingsButton from './Components/Settings';
 
 function App() {
   return (
@@ -8,6 +10,13 @@ function App() {
       <Timer/>
         <div>
           <StartButton/>
+          <PauseButton/>
+        </div>
+        <div>
+          <SettingsButton/>
+        </div>
+        <div>
+
         </div>
     </div>
     
